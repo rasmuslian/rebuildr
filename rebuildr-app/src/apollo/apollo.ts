5f409f66b4cfc99bbcb1eadaa84e6a9020ecd77a
Alfred Yrelin <alfredyrelin@gmail.com>
2024-06-24 09:43:47 +0200

    Update backend url for prod

--- rebuildr-app/src/apollo/apollo.ts
+++ rebuildr-app/src/apollo/apollo.ts
@@ -12,7 +12,7 @@ import { onError } from "@apollo/client/link/error";
 export const isLoggedInVar = makeVar(false);
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:3000/graphql",
+  uri: process.env.NODE_ENV === "development" ? "http://localhost:3000/graphql" : "https://rebuildr-backend-6a7ah.ondigitalocean.app/graphql",
 });
 
 const authLink = setContext(async (_, { headers }) => {
