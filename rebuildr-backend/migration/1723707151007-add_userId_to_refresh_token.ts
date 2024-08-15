40a84096063e8c0d28b9dc360889e69975b6bb89
Emil Stolpe <emil.stolpe@swace.se>
2024-08-15 09:33:41 +0200

    migration needed after adding userid to refresh token

---
+++ rebuildr-backend/migration/1723707151007-add_userId_to_refresh_token.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddUserIdToRefreshToken1723707151007 implements MigrationInterface {
+    name = 'AddUserIdToRefreshToken1723707151007'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "refresh_token" DROP CONSTRAINT "FK_6bbe63d2fe75e7f0ba1710351d4"`);
+        await queryRunner.query(`ALTER TABLE "refresh_token" ALTER COLUMN "user_id" SET NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "refresh_token" ADD CONSTRAINT "FK_6bbe63d2fe75e7f0ba1710351d4" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "refresh_token" DROP CONSTRAINT "FK_6bbe63d2fe75e7f0ba1710351d4"`);
+        await queryRunner.query(`ALTER TABLE "refresh_token" ALTER COLUMN "user_id" DROP NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "refresh_token" ADD CONSTRAINT "FK_6bbe63d2fe75e7f0ba1710351d4" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+    }
+
+}
