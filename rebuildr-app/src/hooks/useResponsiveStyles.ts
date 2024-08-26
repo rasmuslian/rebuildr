1352120cc2609ea4dde58963590e96b0d0241641
Emil Stolpe <emil.stolpe@swace.se>
2024-08-26 14:28:21 +0200

    mimic typing in Stylesheet for more precise typing of styles

--- rebuildr-app/src/hooks/useResponsiveStyles.ts
+++ rebuildr-app/src/hooks/useResponsiveStyles.ts
@@ -5,6 +5,11 @@ import {
   ScreenDimensionsContext,
 } from "src/contexts/screenDimensionsContext";
 
+type ResponsiveStyle<T> = {
+  // eslint-disable-next-line no-unused-vars
+  [P in keyof T]: ViewStyle | TextStyle | ImageStyle | ResponsiveStyleType;
+};
+
 export type ResponsiveStyleType = {
   // eslint-disable-next-line no-unused-vars
   [key in keyof MediaBreakPoints]?: ViewStyle | TextStyle | ImageStyle;
@@ -18,11 +23,10 @@ export const useResponsiveStyle = (
   return mergeStyles(responsiveStyle, breakPoint);
 };
 
-export const useResponsiveStyles = (
-  responsiveStyle: Record<
-    string,
-    (ViewStyle | TextStyle | ImageStyle) & ResponsiveStyleType
-  >,
+export const useResponsiveStyles = <
+  T extends ResponsiveStyle<T> | ResponsiveStyle<any>,
+>(
+  responsiveStyle: T & ResponsiveStyle<any>,
 ) => {
   const breakPoint = useContext(ScreenDimensionsContext);
 
