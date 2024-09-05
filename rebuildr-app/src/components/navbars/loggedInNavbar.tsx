8071158bf32a2679b03387810bb1df760b0d7f65
Emil Stolpe <emil.stolpe@swace.se>
2024-09-05 12:50:12 +0200

    update navbar

--- rebuildr-app/src/components/navbars/loggedInNavbar.tsx
+++ rebuildr-app/src/components/navbars/loggedInNavbar.tsx
@@ -3,9 +3,11 @@ import { StyleSheet, View, Image, Pressable } from "react-native";
 import Colors from "src/styles/colors";
 import { Button } from "../button";
 import logo from "assets/images/logo.png";
-import { useNavigation } from "@react-navigation/native";
+import { Link, useNavigation } from "@react-navigation/native";
 import { Body } from "../texts/text";
 import { UserRoleEnum } from "src/gql/graphql";
+import { Icon } from "../icons/icon";
+import { useResponsiveStyles } from "src/hooks/useResponsiveStyles";
 
 interface LoggedInNavbarProps {
   me: { email: string; role: UserRoleEnum };
@@ -13,29 +15,25 @@ interface LoggedInNavbarProps {
 
 export const LoggedInNavbar = ({ me }: LoggedInNavbarProps) => {
   const { navigate } = useNavigation();
+  const responsiveStyles = useResponsiveStyles(styles);
 
   return (
-    <View
-      style={[
-        styles.container,
-        {
-          backgroundColor:
-            me.role === UserRoleEnum.Admin ? Colors.button.purple : Colors.blue,
-        },
-      ]}
-    >
+    <View style={[styles.container]}>
       <Pressable onPress={() => navigate("Landing")}>
         <Image source={logo} />
       </Pressable>
-      <Body>{me.email}</Body>
       <View style={styles.ctaButtons}>
+        <Link to={{ screen: "Account" }}>
+          <View style={{ flexDirection: "row", alignItems: "center", gap: 4 }}>
+            <Icon iconType="Person" />
+            <Body style={responsiveStyles.removeOnMobile}>{me.email}</Body>
+          </View>
+        </Link>
         <Button
-          onPress={() => navigate("Conversations")}
-          title={"Meddelanden"}
+          onPress={() => navigate("Sell")}
+          title="NY ANNONS"
+          style={responsiveStyles.removeOnMobile}
         />
-        <Button onPress={() => navigate("Account")} icon={"Person"}>
-          <Body>Mitt konto</Body>
-        </Button>
       </View>
     </View>
   );
@@ -47,12 +45,17 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     alignItems: "center",
     flexDirection: "row",
-    width: "100%",
-    backgroundColor: Colors.blue,
+    backgroundColor: Colors.brand,
     paddingHorizontal: 34,
   },
   ctaButtons: {
     flexDirection: "row",
-    gap: 4,
+    alignItems: "center",
+    gap: 20,
+  },
+  removeOnMobile: {
+    small: {
+      display: "none",
+    },
   },
 });
