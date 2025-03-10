3aa975f967a4b585c5e299313b21803bcd7705ed
Emil Stolpe <emil.stolpe@swace.se>
2025-03-10 09:58:25 +0100

    add cards component

---
+++ rebuildr-app/src/utils/enumToText.ts
@@ -0,0 +1,11 @@
+import { ProductConditionEnum } from "../gql/graphql";
+
+// eslint-disable-next-line no-unused-vars
+export const ProductConditionToText: { [key in ProductConditionEnum]: string } =
+  {
+    BAD: "Dåligt skick",
+    GOOD: "Gott skick",
+    NEW: "Nytt skick",
+    OKAY: "Okej skick",
+    VERY_GOOD: "Mycket gott skick",
+  };
