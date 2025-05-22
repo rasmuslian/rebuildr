c180b73d854814646dac1a2144b779c6861f5824
Emil Stolpe <emil.stolpe@swace.se>
2025-05-22 10:25:55 +0200

    fix numberToString for the case where nonDecimal is 0

--- rebuildr-app/utils/number-strings.ts
+++ rebuildr-app/utils/number-strings.ts
@@ -1,6 +1,8 @@
 export const numberToString = (n: number, nrOfDecimals: number) => {
   const nonDecimal = Math.round(n);
-  const decimal = Math.round((n % nonDecimal) * 10 * nrOfDecimals);
+  const decimal = Math.round(
+    (nonDecimal ? n % nonDecimal : n) * 10 * nrOfDecimals,
+  );
 
   return `${nonDecimal},${decimal}`;
 };
