1b22990d20b4d662e945442948cae0a356a0b1fc
Emil Stolpe <emil.stolpe@swace.se>
2025-06-18 16:40:02 +0200

    add placeholder screens

---
+++ rebuildr-app/app/(app)/account/purchases/index.tsx
@@ -0,0 +1,5 @@
+import { Title } from "@components/typography/text";
+
+export default function Purchases() {
+  return <Title size="large">In progress</Title>;
+}
---
+++ rebuildr-app/app/(app)/account/sales/index.tsx
@@ -0,0 +1,5 @@
+import { Title } from "@components/typography/text";
+
+export default function Sales() {
+  return <Title size="large">In progress</Title>;
+}
