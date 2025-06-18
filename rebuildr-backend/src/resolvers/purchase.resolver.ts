d89612dca2d379b134070887a327ad4c162b5aaf
Emil Stolpe <emil.stolpe@swace.se>
2025-06-18 16:37:00 +0200

    move around functions to make the file more clear. Also add new function so that a user can
    see if purchase is shipping
    a seller can mark a purchase a delivered
    a user can get their latest purchase on the product

--- rebuildr-backend/src/resolvers/purchase.resolver.ts
+++ rebuildr-backend/src/resolvers/purchase.resolver.ts
@@ -1,19 +1,26 @@
-import { UseGuards } from '@nestjs/common';
+import { Inject, UseGuards } from '@nestjs/common';
 import {
   Args,
   Field,
   InputType,
   Mutation,
   ObjectType,
+  Parent,
+  Query,
+  ResolveField,
   Resolver,
 } from '@nestjs/graphql';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { AuthedUserType } from 'src/auth/constants';
 import { GqlAuthGuard } from 'src/auth/gql-auth.guard';
 import { CurrentUser } from 'src/decorators/current-user.decorator';
+import { RequestId } from 'src/decorators/request-id.decorator';
 import { Product } from 'src/entities/product.entity';
 import { Purchase } from 'src/entities/purchase.entity';
+import { Review } from 'src/entities/review.entity';
 import { PurchaseService } from 'src/services/purchase.service';
 import { SupportedPaymentMethod } from 'src/services/rocker.service';
+import { Logger } from 'winston';
 
 @InputType()
 class PurchaseProductInput {
@@ -39,9 +46,37 @@ class AcceptPurchaseInput {
   purchaseId: string;
 }
 
+@InputType()
+export class LatestPurchaseInput {
+  @Field()
+  buyerId: string;
+  @Field()
+  sellerId: string;
+  @Field()
+  productId: string;
+}
+
+@InputType()
+class MarkPurchaseAsDeliveredInput {
+  @Field()
+  purchaseId: string;
+}
+
 @Resolver(() => Purchase)
 export class PurchaseResolver {
-  constructor(private purchaseService: PurchaseService) {}
+  constructor(
+    private purchaseService: PurchaseService,
+    @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
+  ) {}
+
+  @Query(() => Purchase, { nullable: true })
+  @UseGuards(GqlAuthGuard)
+  async latestPurchase(
+    @Args('input') input: LatestPurchaseInput,
+    @CurrentUser() user: AuthedUserType,
+  ) {
+    return await this.purchaseService.latestPurchase(input, user.id);
+  }
 
   @Mutation(() => PurchaseProductResponse)
   @UseGuards(GqlAuthGuard)
@@ -66,5 +101,34 @@ export class PurchaseResolver {
       input.purchaseId,
       _user.id,
     );
+  }
+
+  @Mutation(() => Purchase)
+  @UseGuards(GqlAuthGuard)
+  async markPurchaseAsDelivered(
+    @Args('input') input: MarkPurchaseAsDeliveredInput,
+    @CurrentUser() user: AuthedUserType,
+    @RequestId() requestId: string,
+  ) {
+    const childLogger = this.logger.child({
+      requestId,
+      userId: user.id,
+      purchaseId: input.purchaseId,
+    });
+    return await this.purchaseService.markAsDelivered(
+      input.purchaseId,
+      user.id,
+      childLogger,
+    );
+  }
+
+  @ResolveField(() => Boolean)
+  async isShipping(@Parent() purchase: Purchase) {
+    return this.purchaseService.isShipping(purchase);
+  }
+
+  @ResolveField(() => [Review])
+  async reviews(@Parent() purchase: Purchase) {
+    return this.purchaseService.reviews(purchase);
   }
 }
