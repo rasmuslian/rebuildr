c8c7d9054c0339c43529bad51f7be76049facda2
Emil Stolpe <emil.stolpe@swace.se>
2025-06-24 09:19:55 +0200

    reb-134: use openstreetmap since it's free

--- rebuildr-app/components/maps/map.tsx
+++ rebuildr-app/components/maps/map.tsx
@@ -52,7 +52,8 @@ export const Map = ({
         minZoom={0}
         maxZoom={20}
         // attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.{ext}"
+        // url="https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.{ext}"
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         ext="png"
       />
       <InnerMap
