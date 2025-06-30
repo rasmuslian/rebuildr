271b2b82a5c0e607459d4eba0487516e92d9f537
Emil Stolpe <emil.stolpe@swace.se>
2025-06-30 09:07:33 +0200

    user must add contact information when they set shipping on a product

--- rebuildr-app/components/transport/shipping.tsx
+++ rebuildr-app/components/transport/shipping.tsx
@@ -3,15 +3,22 @@ import {
   ShippingProviderEnum,
   ShippingQueryQuery,
   ShippingQueryQueryVariables,
+  ShippingUpdateUserMutation,
+  ShippingUpdateUserMutationVariables,
+  UpdateShippingMutation,
+  UpdateShippingMutationVariables,
+  User,
 } from "@/gql/graphql";
 import { gql, useMutation, useSuspenseQuery } from "@apollo/client";
+import { Button } from "@components/buttons/button";
 import { Radio } from "@components/controls/radio";
 import { Divider } from "@components/dividers/divider";
+import { Form } from "@components/forms/form";
 import { ToggleCard } from "@components/toggle-card/toggle-card";
 import { Body, Title } from "@components/typography/text";
 import { borderRadius } from "@constants/sizes";
 import { useThemeColor } from "@hooks/useThemeColor";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { View } from "react-native";
 import { Pressable } from "react-native-gesture-handler";
 
@@ -32,6 +39,14 @@ const SHIPPING_QUERY = gql`
         provider
       }
     }
+    me {
+      id
+      name
+      address
+      postCode
+      phoneNumber
+      city
+    }
   }
 `;
 
@@ -51,22 +66,81 @@ const UPDATE_SHIPPING = gql`
   }
 `;
 
+const SHIPPING_UPDATE_USER = gql`
+  mutation ShippingUpdateUser($input: UpdateUserInput!) {
+    updateUser(input: $input) {
+      user {
+        id
+        name
+        address
+        postCode
+        phoneNumber
+        city
+      }
+    }
+  }
+`;
 type Props = {
   productId: string;
+  onShippingValid: (valid: boolean) => void;
+  onShippingSelected: (selected: boolean) => void;
+  shippingSelected: boolean;
 };
 
-export const Shipping = ({ productId }: Props) => {
+export const Shipping = ({
+  productId,
+  onShippingValid,
+  onShippingSelected,
+  shippingSelected,
+}: Props) => {
   const { data } = useSuspenseQuery<
     ShippingQueryQuery,
     ShippingQueryQueryVariables
   >(SHIPPING_QUERY, { variables: { input: { id: productId } } });
-  const [updateShipping] = useMutation(UPDATE_SHIPPING);
-  const [shippingEnabled, setShippingEnabled] = useState(
-    !!data.product.shippingPrices?.length,
+  const [name, setName] = useState<string | null | undefined>(data.me.name);
+  const [phoneNumber, setPhoneNumber] = useState<string | null | undefined>(
+    data.me.phoneNumber,
   );
+  const [address, setAddress] = useState<string | null | undefined>(
+    data.me.address,
+  );
+  const [postCode, setPostCode] = useState<string | null | undefined>(
+    data.me.postCode,
+  );
+  const [city, setCity] = useState<string | null | undefined>(data.me.city);
+  const [updateShipping] = useMutation<
+    UpdateShippingMutation,
+    UpdateShippingMutationVariables
+  >(UPDATE_SHIPPING);
+  const [updateUser, { loading: loadingUpdateUser }] = useMutation<
+    ShippingUpdateUserMutation,
+    ShippingUpdateUserMutationVariables
+  >(SHIPPING_UPDATE_USER);
   const colors = useThemeColor();
   const [provider, setProvider] = useState<ShippingProviderEnum>(
     ShippingProviderEnum.Postnord,
+  );
+
+  const shippingValidHandler = (
+    user: Partial<User>,
+    shippingPrices?: Partial<ShippingPrice>[] | null,
+  ) => {
+    if (
+      user.address &&
+      user.name &&
+      user.phoneNumber &&
+      user.postCode &&
+      user.city &&
+      shippingPrices?.length
+    ) {
+      onShippingValid(true);
+      return;
+    }
+    onShippingValid(false);
+  };
+  useEffect(
+    () => shippingValidHandler(data.me, data.product.shippingPrices),
+    [data],
   );
 
   const onSelectPrice = (id: string) => {
@@ -77,10 +151,13 @@ export const Shipping = ({ productId }: Props) => {
           shippingPriceIds: [id],
         },
       },
+      onCompleted: (d) => {
+        shippingValidHandler(data.me, d.updateProduct.product.shippingPrices);
+      },
     });
   };
   const onSelectShipping = () => {
-    if (shippingEnabled) {
+    if (shippingSelected) {
       updateShipping({
         variables: {
           input: {
@@ -89,12 +166,29 @@ export const Shipping = ({ productId }: Props) => {
           },
         },
         onCompleted: () => {
-          setShippingEnabled(false);
+          onShippingSelected(false);
         },
       });
     } else {
-      setShippingEnabled(true);
+      onShippingSelected(true);
     }
+  };
+  const onSaveDetails = () => {
+    updateUser({
+      variables: {
+        input: {
+          id: data.me.id,
+          name,
+          address,
+          postCode,
+          phoneNumber,
+          city,
+        },
+      },
+      onCompleted: (d) => {
+        shippingValidHandler(d.updateUser.user, data.product.shippingPrices);
+      },
+    });
   };
 
   const isSelected = (shippingPrice: ShippingPrice) => {
@@ -108,7 +202,7 @@ export const Shipping = ({ productId }: Props) => {
       title="Fraktleverans"
       description="Du skickar produkten till köparen via ett fraktbolag."
       onPress={onSelectShipping}
-      enabled={shippingEnabled}
+      enabled={shippingSelected}
     >
       <Divider />
 
@@ -174,6 +268,64 @@ export const Shipping = ({ productId }: Props) => {
                 </View>
               </Pressable>
             ))}
+        </View>
+        <Divider />
+        <View>
+          <Title size="medium">Din adress </Title>
+          <Body size="medium" style={{ marginTop: 4 }}>
+            Den adress vi använder för eventuella returer.
+          </Body>
+          <Form
+            style={{ gap: 16, marginTop: 16 }}
+            fields={[
+              {
+                type: "text",
+                value: name ?? data.me.name ?? "",
+                onChange: (t) => setName(t),
+                heading: "För- och efternamn",
+              },
+              {
+                type: "text",
+                value: phoneNumber ?? data.me.phoneNumber ?? "",
+                onChange: (t) => setPhoneNumber(t),
+                heading: "Telefonnummer",
+              },
+              {
+                type: "text",
+                value: address ?? data.me.address ?? "",
+                onChange: (t) => setAddress(t),
+                heading: "Gatuadress",
+              },
+              {
+                type: "text",
+                value: postCode ?? data.me.postCode ?? "",
+                onChange: (t) => setPostCode(t),
+                heading: "Postnummer",
+                horizontalSize: 1,
+              },
+              {
+                type: "text",
+                value: city ?? data.me.city ?? "",
+                onChange: (t) => setCity(t),
+                heading: "Stad",
+                horizontalSize: 2,
+              },
+            ]}
+          />
+          <Button
+            style={{ marginTop: 16 }}
+            label="Spara"
+            onPress={onSaveDetails}
+            loading={loadingUpdateUser}
+          />
+          <Body
+            size="small"
+            color="secondary"
+            style={{ marginTop: 12, textAlign: "center" }}
+          >
+            Vi sparar den här adressen i dina kontoinställningar, så slipper du
+            fylla i detta igen.
+          </Body>
         </View>
       </View>
     </ToggleCard>
