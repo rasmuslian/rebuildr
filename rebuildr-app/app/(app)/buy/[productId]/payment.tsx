a5eaa09df8403fc86a916c6d11011e342f0cfb60
Emil Stolpe <emil.stolpe@swace.se>
2025-07-14 15:27:20 +0200

    refactor swish bottom sheet

--- rebuildr-app/app/(app)/buy/[productId]/payment.tsx
+++ rebuildr-app/app/(app)/buy/[productId]/payment.tsx
@@ -5,9 +5,6 @@ import {
   BuyProductPaymentQueryVariables,
   PaymentMethod,
   PaymentTypeEnum,
-  PollSwishQuery,
-  PollSwishQueryVariables,
-  PurchaseStatusEnum,
   ShippingProviderEnum,
   TransportationEnum,
 } from "@/gql/graphql";
@@ -20,11 +17,11 @@ import { Form } from "@components/forms/form";
 import { TextInput } from "@components/forms/textInput";
 import { ProgressHeader } from "@components/navigation/headers/progress-header";
 import { ScreenLayout } from "@components/screen-layout/screen-layout";
-import { Body, Display, Headline, Title } from "@components/typography/text";
+import { Body, Display, Title } from "@components/typography/text";
 import { borderRadius } from "@constants/sizes";
 import { useThemeColor } from "@hooks/useThemeColor";
 import { router, useLocalSearchParams } from "expo-router";
-import React, { ReactElement, useEffect, useRef, useState } from "react";
+import React, { ReactElement, useState } from "react";
 import { Platform, View } from "react-native";
 import { Image } from "expo-image";
 import SwishPaymentOption from "@assets/images/swish-payment-option.png";
@@ -34,15 +31,12 @@ import TrustlyPaymentOption from "@assets/images/trustly-payment-option.png";
 import { LoadingSpinner } from "@components/loading-spinner/loading-spinner";
 import { ToggleCard } from "@components/toggle-card/toggle-card";
 import { VerifyBottomSheet } from "@components/bank-id/verify-bottom-sheet";
-import { BottomSheetModal } from "@gorhom/bottom-sheet";
-import { BottomSheet } from "@components/bottom-sheet/bottom-sheet";
-import SwishImage from "@assets/images/swish-no-border.png";
-import { Check } from "@components/controls/check";
 import {
   TransportationString,
   transportationStringToEnum,
 } from "@/utils/transportationMethods";
 import { StripeBottomSheet } from "@components/payment/stripe-bottom-sheet";
+import { SwishBottomSheet } from "@components/payment/swish-bottom-sheet";
 
 const BUY_PRODUCT_PAYMENT = gql`
   query BuyProductPayment($input: GetProductInput!) {
@@ -441,130 +435,5 @@ const PaymentCard = ({
       onPress={onToggle}
       enabled={toggledOn}
     />
-  );
-};
-
-const POLL_SWISH = gql`
-  query PollSwish($input: GetPurchaseInput!) {
-    purchase(input: $input) {
-      id
-      status
-    }
-  }
-`;
-
-type SwishBottomSheetProps = {
-  price: number;
-  productId: string;
-  purchaseId: string;
-  show: boolean;
-  onDismiss: () => void;
-};
-
-const SwishBottomSheet = ({
-  price,
-  productId,
-  purchaseId,
-  show,
-  onDismiss,
-}: SwishBottomSheetProps) => {
-  const colors = useThemeColor();
-  const ref = useRef<BottomSheetModal>(null);
-  useQuery<PollSwishQuery, PollSwishQueryVariables>(POLL_SWISH, {
-    variables: { input: { id: purchaseId } },
-    pollInterval: 1000,
-    notifyOnNetworkStatusChange: true,
-    onCompleted: (data) => {
-      if (data.purchase.status === PurchaseStatusEnum.PaymentAccepted) {
-        ref.current?.dismiss();
-        onDismiss();
-        router.replace({
-          pathname: "/buy/[productId]/success",
-          params: { productId, purchaseId },
-        });
-      }
-    },
-  });
-
-  useEffect(() => {
-    if (show) {
-      ref.current?.present();
-    } else {
-      ref.current?.dismiss();
-    }
-  }, [show]);
-
-  return (
-    <BottomSheet name="Swish" title="Bekräfta köp" ref={ref} screenHeight>
-      <>
-        <View style={{ gap: 24 }}>
-          <View
-            style={{
-              paddingVertical: 24,
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Image
-              source={SwishImage.uri}
-              style={{ width: 187, height: 187 }}
-            />
-          </View>
-          <View>
-            <Display size="small" style={{ textAlign: "center" }}>
-              Nästan klart!
-            </Display>
-            <Display size="small" style={{ textAlign: "center" }}>
-              Öppna Swish för att betala
-            </Display>
-          </View>
-
-          <Divider />
-
-          <View style={{ gap: 16 }}>
-            <Headline size="small">Såhär gör du:</Headline>
-            <View
-              style={{ flexDirection: "row", alignItems: "center", gap: 8 }}
-            >
-              <Check
-                checkColor="primaryDark"
-                selected
-                color={colors.navigation.hovered}
-              />
-              <Body size="medium">Öppna din Swish-app</Body>
-            </View>
-            <View
-              style={{ flexDirection: "row", alignItems: "center", gap: 8 }}
-            >
-              <Check
-                selected
-                color={colors.navigation.hovered}
-                checkColor="primaryDark"
-              />
-              <Body size="medium">
-                Dubbelkolla att det står <Body size="medium">{price} kr</Body>{" "}
-                och att mottagaren är Rocker
-              </Body>
-            </View>
-            <View
-              style={{ flexDirection: "row", alignItems: "center", gap: 8 }}
-            >
-              <Check
-                checkColor="primaryDark"
-                selected
-                color={colors.navigation.hovered}
-              />
-              <Body size="medium">Godkänn betalningen i Swish</Body>
-            </View>
-          </View>
-        </View>
-
-        <Button
-          label="Avbryt"
-          onPress={() => onDismiss()}
-          style={{ marginTop: 82 }}
-        />
-      </>
-    </BottomSheet>
   );
 };
---
+++ rebuildr-app/components/payment/swish-bottom-sheet.tsx
@@ -0,0 +1,143 @@
+import {
+  PollSwishQuery,
+  PollSwishQueryVariables,
+  PurchaseStatusEnum,
+} from "@/gql/graphql";
+import { gql, useQuery } from "@apollo/client";
+import { BottomSheet } from "@components/bottom-sheet/bottom-sheet";
+import { Divider } from "@components/dividers/divider";
+import { Display, Headline, Body } from "@components/typography/text";
+import { BottomSheetModal } from "@gorhom/bottom-sheet";
+import { useThemeColor } from "@hooks/useThemeColor";
+import { router } from "expo-router";
+import { useRef, useEffect } from "react";
+import { View } from "react-native";
+import { Image } from "expo-image";
+import { Check } from "@components/controls/check";
+import SwishImage from "@assets/images/swish-no-border.png";
+import { Button } from "@components/buttons/button";
+
+const POLL_SWISH = gql`
+  query PollSwish($input: GetPurchaseInput!) {
+    purchase(input: $input) {
+      id
+      status
+    }
+  }
+`;
+
+type SwishBottomSheetProps = {
+  price: number;
+  productId: string;
+  purchaseId: string;
+  show: boolean;
+  onDismiss: () => void;
+};
+
+export const SwishBottomSheet = ({
+  price,
+  productId,
+  purchaseId,
+  show,
+  onDismiss,
+}: SwishBottomSheetProps) => {
+  const colors = useThemeColor();
+  const ref = useRef<BottomSheetModal>(null);
+  useQuery<PollSwishQuery, PollSwishQueryVariables>(POLL_SWISH, {
+    variables: { input: { id: purchaseId } },
+    pollInterval: 1000,
+    notifyOnNetworkStatusChange: true,
+    onCompleted: (data) => {
+      if (data.purchase.status === PurchaseStatusEnum.PaymentAccepted) {
+        ref.current?.dismiss();
+        onDismiss();
+        router.replace({
+          pathname: "/buy/[productId]/success",
+          params: { productId, purchaseId },
+        });
+      }
+    },
+  });
+
+  useEffect(() => {
+    if (show) {
+      ref.current?.present();
+    } else {
+      ref.current?.dismiss();
+    }
+  }, [show]);
+
+  return (
+    <BottomSheet name="Swish" title="Bekräfta köp" ref={ref} screenHeight>
+      <>
+        <View style={{ gap: 24 }}>
+          <View
+            style={{
+              paddingVertical: 24,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Image
+              source={SwishImage.uri}
+              style={{ width: 187, height: 187 }}
+            />
+          </View>
+          <View>
+            <Display size="small" style={{ textAlign: "center" }}>
+              Nästan klart!
+            </Display>
+            <Display size="small" style={{ textAlign: "center" }}>
+              Öppna Swish för att betala
+            </Display>
+          </View>
+
+          <Divider />
+
+          <View style={{ gap: 16 }}>
+            <Headline size="small">Såhär gör du:</Headline>
+            <View
+              style={{ flexDirection: "row", alignItems: "center", gap: 8 }}
+            >
+              <Check
+                checkColor="primaryDark"
+                selected
+                color={colors.navigation.hovered}
+              />
+              <Body size="medium">Öppna din Swish-app</Body>
+            </View>
+            <View
+              style={{ flexDirection: "row", alignItems: "center", gap: 8 }}
+            >
+              <Check
+                selected
+                color={colors.navigation.hovered}
+                checkColor="primaryDark"
+              />
+              <Body size="medium">
+                Dubbelkolla att det står <Body size="medium">{price} kr</Body>{" "}
+                och att mottagaren är Rocker
+              </Body>
+            </View>
+            <View
+              style={{ flexDirection: "row", alignItems: "center", gap: 8 }}
+            >
+              <Check
+                checkColor="primaryDark"
+                selected
+                color={colors.navigation.hovered}
+              />
+              <Body size="medium">Godkänn betalningen i Swish</Body>
+            </View>
+          </View>
+        </View>
+
+        <Button
+          label="Avbryt"
+          onPress={() => onDismiss()}
+          style={{ marginTop: 82 }}
+        />
+      </>
+    </BottomSheet>
+  );
+};
