43db6d92dde8d4650ce67cf6df8a0355e7accd4b
Emil Stolpe <emil.stolpe@swace.se>
2025-07-17 10:20:22 +0200

    remove unused props from accordionSection

--- rebuildr-app/components/sections/accordion-section.tsx
+++ rebuildr-app/components/sections/accordion-section.tsx
@@ -3,25 +3,11 @@ import { SectionHeader } from "./section-header";
 import { View } from "react-native";
 
 type Props = {
-  title?: string;
-  collapsedTitle?: string;
-  collapsedText?: string;
-  expandedTitle?: string;
+  title: string;
   initialOpen?: boolean;
 } & PropsWithChildren;
-export const AccordionSection = ({
-  title,
-  collapsedTitle,
-  collapsedText,
-  expandedTitle,
-  initialOpen,
-  children,
-}: Props) => {
+export const AccordionSection = ({ title, initialOpen, children }: Props) => {
   const [isOpen, setIsOpen] = useState(!!initialOpen);
-
-  const headerTitle = isOpen
-    ? (expandedTitle ?? title)
-    : (collapsedTitle ?? title);
 
   return (
     <View style={{ gap: 16 }}>
@@ -29,7 +15,7 @@ export const AccordionSection = ({
         icon={isOpen ? "chevronUp" : "chevronDown"}
         onPress={() => setIsOpen(!isOpen)}
       >
-        {headerTitle ?? ""}
+        {title}
       </SectionHeader>
       {isOpen ? children : null}
     </View>
