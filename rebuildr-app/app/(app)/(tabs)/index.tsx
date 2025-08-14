51c4fc356ce2257b3c4364f47c1d50d01616dfaa
Aliyawer Qambari <aliyawerqambari@gmail.com>
2025-08-14 13:27:11 +0200

    Animat hero headline on scroll

--- rebuildr-app/app/(app)/(tabs)/index.tsx
+++ rebuildr-app/app/(app)/(tabs)/index.tsx
@@ -1,44 +1,49 @@
-import { View } from "react-native";
+import { View, Animated } from "react-native";
 import TopBar from "@components/navigation/top-bar";
 import Hero from "@components/hero/hero";
-import { ScreenLayout } from "@components/screen-layout/screen-layout";
 import Footer from "@components/navigation/footer";
 import { TrendingNow } from "@components/trending-now/trending-now";
-import { Divider } from "@components/dividers/divider";
-import { Headline } from "@components/typography/text";
 import { NewArrivalsNearYou } from "@components/new-arrivals-near-you/new-arrivals-near-you";
 import { ForTheSeason } from "@components/for-the-season/for-the-season";
 import { SaleBanner } from "@components/sale-banner/sale-banner";
 import { RootCategories } from "@components/root-categories/root-categories";
+import React, { useRef } from "react";
+import { useThemeColor } from "@hooks/useThemeColor";
 
 export default function Landing() {
-  const Header = () => (
-    <View>
-      <TopBar />
-      <Hero />
-    </View>
-  );
+  const scrollY = useRef(new Animated.Value(0)).current;
+  const colors = useThemeColor();
 
   return (
-    <ScreenLayout
-      headerComponent={<Header />}
-      headerStyle={{ paddingHorizontal: 0 }}
-      footerComponent={<Footer />}
-      isStickyFooter={false}
-    >
-      <View>
-        <RootCategories />
-        <NewArrivalsNearYou />
-        <ForTheSeason />
-        <SaleBanner />
-        <View style={{ flexDirection: "column", gap: 16, paddingVertical: 16 }}>
-          <Headline size="small">Du kanske också gillar</Headline>
-          <Divider />
-          <Headline size="small">Nytt från din senaste söknin</Headline>
-          <Divider />
+    <View style={{ flex: 1 }}>
+      <TopBar />
+      <Hero scrollY={scrollY} />
+
+      <Animated.ScrollView
+        scrollEventThrottle={16}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+          { useNativeDriver: true },
+        )}
+      >
+        <View
+          style={{
+            backgroundColor: colors.background.neutral,
+            flexGrow: 1,
+            paddingHorizontal: 16,
+            paddingBottom: 32,
+            paddingTop: 24,
+          }}
+        >
+          <RootCategories />
+          <NewArrivalsNearYou />
+          <ForTheSeason />
+          <SaleBanner />
+          <TrendingNow />
         </View>
-        <TrendingNow />
-      </View>
-    </ScreenLayout>
+
+        <Footer />
+      </Animated.ScrollView>
+    </View>
   );
 }
--- rebuildr-app/components/hero/hero.tsx
+++ rebuildr-app/components/hero/hero.tsx
@@ -1,11 +1,17 @@
-import { View, ImageBackground } from "react-native";
+import { View, ImageBackground, Animated } from "react-native";
 import { useThemeColor } from "@hooks/useThemeColor";
 import { Headline } from "@components/typography/text";
 import { SearchBar } from "@components/search/search-bar";
 import { useDebounceCallback } from "usehooks-ts";
+import React, { useState } from "react";
 
-export default function Hero() {
+type Props = {
+  scrollY: Animated.Value;
+};
+
+export default function Hero({ scrollY }: Props) {
   const colors = useThemeColor();
+  const [headlineHeight, setHeadlineHeight] = useState(0);
 
   const onChangeText = useDebounceCallback((value) => {
     // TODO: Call api endpoint.
@@ -28,12 +34,34 @@ export default function Hero() {
           paddingHorizontal: 16,
           display: "flex",
           flexDirection: "column",
-          gap: 12,
         }}
       >
-        <Headline style={{ color: colors.logo.background }}>
-          Sveriges marknadsplats för återbrukat byggmaterial
-        </Headline>
+        <Animated.View
+          style={{
+            opacity: scrollY.interpolate({
+              inputRange: [0, headlineHeight],
+              outputRange: [1, 0],
+              extrapolate: "clamp",
+            }),
+            height: scrollY.interpolate({
+              inputRange: [0, headlineHeight],
+              outputRange: [headlineHeight, 0],
+              extrapolate: "clamp",
+            }),
+            overflow: "hidden",
+          }}
+        >
+          <Headline
+            onLayout={(e) => setHeadlineHeight(e.nativeEvent.layout.height)}
+            size="small"
+            style={{
+              color: colors.logo.background,
+              paddingBottom: 12,
+            }}
+          >
+            Sveriges marknadsplats för återbrukat byggmaterial
+          </Headline>
+        </Animated.View>
 
         <SearchBar
           style={{ borderBottomWidth: 0 }}
