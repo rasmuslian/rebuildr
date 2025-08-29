2817c0aeb59e33f87d4311e99b029f27846e81eb
Emil Stolpe <emil.stolpe@swace.se>
2025-08-29 09:04:26 +0200

    add ProductHeader component

---
+++ rebuildr-app/components/navigation/headers/product-header.tsx
@@ -0,0 +1,100 @@
+import { Badge } from "@components/badges/badge";
+import { Body, Label, Title } from "@components/typography/text";
+import { View, StyleSheet } from "react-native";
+import { Image } from "expo-image";
+import {
+  ProductConditionEnum,
+  ProductStatusEnum,
+  QuantityUnitEnum,
+} from "@/gql/graphql";
+import { borderRadius } from "@constants/sizes";
+import { Divider } from "@components/dividers/divider";
+import { ComponentProps } from "react";
+import DeletedProduct from "@assets/images/deleted-product.png";
+import { quantities } from "@constants/quantities";
+import { conditions } from "@constants/conditions";
+
+type Props = {
+  title: string;
+  price: number;
+  condition?: ProductConditionEnum;
+  quantity?: number | null;
+  quantityUnit?: QuantityUnitEnum | null;
+  statusBadgeProps?: ComponentProps<typeof Badge> | null;
+  imageUrl?: string;
+  status: ProductStatusEnum;
+};
+
+export const ProductHeader = ({
+  title,
+  price,
+  condition,
+  quantity,
+  quantityUnit: _quantityUnit,
+  statusBadgeProps,
+  imageUrl,
+  status,
+}: Props) => {
+  const quantityUnit = _quantityUnit ?? QuantityUnitEnum.Amount;
+  return (
+    <View style={{ gap: 16 }}>
+      <View style={{ flexDirection: "row", gap: 16 }}>
+        <View
+          style={{
+            alignItems: "flex-start",
+            flex: 1,
+          }}
+        >
+          <Title size="small" numberOfLines={1}>
+            {title}
+          </Title>
+          {condition && quantity && (
+            <Body color="secondary" size="small" numberOfLines={1}>
+              {quantity} {quantities[quantityUnit].short} •{" "}
+              {conditions[condition].name}
+            </Body>
+          )}
+          <Label size="large" style={{ marginTop: 2 }}>
+            {price} kr
+          </Label>
+          {statusBadgeProps && (
+            <View style={{ marginTop: 8, flex: 1 }}>
+              <Badge {...statusBadgeProps} />
+            </View>
+          )}
+        </View>
+        <View>
+          <Image
+            source={{
+              uri:
+                status === ProductStatusEnum.Deleted
+                  ? DeletedProduct.uri
+                  : imageUrl,
+            }}
+            style={{
+              width: 64,
+              height: 64,
+              borderRadius: borderRadius.small,
+            }}
+          />
+          {status === ProductStatusEnum.Sold && (
+            <View
+              style={{
+                ...StyleSheet.absoluteFillObject,
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "#00000080",
+                borderRadius: borderRadius.medium,
+              }}
+            >
+              <Label size="large" style={{ color: "white" }}>
+                Såld
+              </Label>
+            </View>
+          )}
+        </View>
+      </View>
+      <Divider />
+    </View>
+  );
+};
