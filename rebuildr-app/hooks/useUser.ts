8af7711af5d21ba9f6e508cb57d363b88261eff4
Aliyawer Qambari <aliyawerqambari@gmail.com>
2025-09-01 10:58:14 +0200

    Fix hinting error

--- rebuildr-app/hooks/useUser.ts
+++ rebuildr-app/hooks/useUser.ts
@@ -1,6 +1,5 @@
-import { useReactiveVar } from "@apollo/client";
+import { useReactiveVar, gql, useQuery } from "@apollo/client";
 import { isLoggedInVar } from "@/apollo/config";
-import { gql, useQuery } from "@apollo/client";
 import { GetMeQuery, GetMeQueryVariables } from "@/gql/graphql";
 
 const GET_ME = gql`
