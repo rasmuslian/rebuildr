7839fe4f6f4207996f525dfe1a11ae6f89df4754
Emil Stolpe <emil.stolpe@swace.se>
2025-09-22 16:21:27 +0200

    fix error reading from undefined

--- rebuildr-app/components/preview-product/image-carousel.tsx
+++ rebuildr-app/components/preview-product/image-carousel.tsx
@@ -26,7 +26,7 @@ export const ImageCarousel = ({
   // Must be a ref or else Flatlist throws error
   const onViewRef = useRef(
     ({ viewableItems }: { viewableItems: ViewToken<File>[] }) => {
-      if (viewableItems[0].index !== null) {
+      if (viewableItems[0]?.index !== null) {
         setVisibleIndex(viewableItems[0].index);
       }
     },
