ebff7fd7191634c307d3845c4f76f3aa86c8e8ce
Aliyawer Qambari <aliyawerqambari@gmail.com>
2025-09-25 14:12:09 +0200

    Read app footer data from api

--- rebuildr-app/components/navigation/footer.tsx
+++ rebuildr-app/components/navigation/footer.tsx
@@ -1,201 +1,42 @@
-import {
-  Pressable,
-  View,
-  TouchableOpacity,
-  FlatList,
-  Linking,
-} from "react-native";
+import { Pressable, View, FlatList } from "react-native";
 import React from "react";
 import { Body, Label, Title } from "@components/typography/text";
 import { Icon } from "@icons/icon";
 import { Logo } from "@components/logo/logo";
 import { primitives } from "@constants/colors";
+import { gql, useQuery } from "@apollo/client";
+import { Link } from "expo-router";
+import {
+  ListFooterSectionQuery,
+  ListFooterSectionQueryVariables,
+} from "@/gql/graphql";
 
-// Mock data - Start
-type LinkGroup = {
-  title: string;
-  links: {
-    label: string;
-    href: string;
-  }[];
-};
-
-const linkGroups: LinkGroup[] = [
-  {
-    title: "INSPIRATION",
-    links: [
-      {
-        label: "Byggtips & tricks",
-        href: "#",
-      },
-      {
-        label: "Bygginspiration",
-        href: "#",
-      },
-      {
-        label: "Byggbloggen",
-        href: "#",
-      },
-      {
-        label: "Nyhetsbrev",
-        href: "#",
-      },
-      {
-        label: "Välgörenhet",
-        href: "#",
-      },
-    ],
-  },
-  {
-    title: "Sälj med Rebuildr",
-    links: [
-      {
-        label: "Sälj som privatperson",
-        href: "#",
-      },
-      {
-        label: "Sälj som företag",
-        href: "#",
-      },
-      {
-        label: "Öppna butik",
-        href: "#",
-      },
-      {
-        label: "Lager för företag",
-        href: "#",
-      },
-      {
-        label: "Priser",
-        href: "#",
-      },
-    ],
-  },
-  {
-    title: "Köpa",
-    links: [
-      {
-        label: "Handla på Rebuildr",
-        href: "#",
-      },
-      {
-        label: "Köparskydd",
-        href: "#",
-      },
-      {
-        label: "Kategorier",
-        href: "#",
-      },
-      {
-        label: "Köp nu – betala senare",
-        href: "#",
-      },
-      {
-        label: "Populära varumärken",
-        href: "#",
-      },
-    ],
-  },
-  {
-    title: "Kundservice",
-    links: [
-      {
-        label: "Regler",
-        href: "#",
-      },
-      {
-        label: "Säkerhet",
-        href: "#",
-      },
-      {
-        label: "FAQ",
-        href: "#",
-      },
-      {
-        label: "Kundservice & kontakt",
-        href: "#",
-      },
-      {
-        label: "Press",
-        href: "#",
-      },
-      {
-        label: "Integritetsinställningar",
-        href: "#",
-      },
-    ],
-  },
-  {
-    title: "Information",
-    links: [
-      {
-        label: "Användaravtal",
-        href: "#",
-      },
-      {
-        label: "Integritetspolicy",
-        href: "#",
-      },
-      {
-        label: "Cookies",
-        href: "#",
-      },
-      {
-        label: "Om Rebuildr",
-        href: "#",
-      },
-      {
-        label: "Jobba hos oss",
-        href: "#",
-      },
-    ],
-  },
-];
-// Mock data - End
+const LIST_FOOTER_SECTION = gql`
+  query ListFooterSection {
+    listFooterSection {
+      id
+      title
+      orderIndex
+      articleFooterSections {
+        articleId
+        footerSectionId
+        orderIndex
+        article {
+          id
+          title
+        }
+      }
+    }
+  }
+`;
 
 export default function Footer() {
-  const renderLinkGroup = ({
-    item,
-    index,
-  }: {
-    item: LinkGroup;
-    index: number;
-  }) => {
-    const itemsInLastRow = linkGroups.length % 2 || 2;
-    const isLastRow = index >= linkGroups.length - itemsInLastRow;
+  const { data } = useQuery<
+    ListFooterSectionQuery,
+    ListFooterSectionQueryVariables
+  >(LIST_FOOTER_SECTION);
 
-    return (
-      <View
-        style={{
-          flex: 1,
-          marginBottom: isLastRow ? 24 : 48,
-        }}
-      >
-        <Title size="medium" color="primaryLight" style={{ marginBottom: 16 }}>
-          {item.title}
-        </Title>
-
-        {item.links.map((link, linkIndex) => {
-          const isLastLink = linkIndex === item.links.length - 1;
-
-          return (
-            <TouchableOpacity
-              key={linkIndex}
-              onPress={() => Linking.openURL(link.href)}
-            >
-              <Label
-                size="medium"
-                color="primaryLight"
-                style={{ marginBottom: isLastLink ? 0 : 16 }}
-              >
-                {link.label}
-              </Label>
-            </TouchableOpacity>
-          );
-        })}
-      </View>
-    );
-  };
+  const footerSections = data?.listFooterSection ?? [];
 
   return (
     <View
@@ -207,11 +48,50 @@ export default function Footer() {
       }}
     >
       <FlatList
-        data={linkGroups}
+        data={footerSections}
         keyExtractor={(item) => item.title}
         numColumns={2}
         columnWrapperStyle={{ justifyContent: "space-between" }}
-        renderItem={renderLinkGroup}
+        renderItem={({ index, item: { articleFooterSections, title } }) => {
+          const itemsInLastRow = footerSections.length % 2 || 2;
+          const isLastSection = index >= footerSections.length - itemsInLastRow;
+
+          return (
+            <View
+              style={{
+                flex: 1,
+                marginBottom: isLastSection ? 24 : 48,
+              }}
+            >
+              <Title
+                size="medium"
+                color="primaryLight"
+                style={{ marginBottom: 16 }}
+              >
+                {title}
+              </Title>
+
+              {articleFooterSections.map(({ article }, position, array) => {
+                const isLastRow = position === array.length - 1;
+
+                return (
+                  <Link
+                    key={article.id}
+                    style={{ marginBottom: isLastRow ? 0 : 16 }}
+                    href={{
+                      pathname: "/(app)/article/[articleId]",
+                      params: { articleId: article.id, title: article.title },
+                    }}
+                  >
+                    <Label size="medium" color="primaryLight">
+                      {article.title}
+                    </Label>
+                  </Link>
+                );
+              })}
+            </View>
+          );
+        }}
       />
 
       <View style={{ flexDirection: "column", gap: 16 }}>
