0211429e57db76c264bdc21f77eef9bd07475024
Emil Stolpe <emil.stolpe@swace.se>
2025-10-01 16:20:07 +0200

    reb-309: update condition copy upon request

--- rebuildr-app/constants/conditions.ts
+++ rebuildr-app/constants/conditions.ts
@@ -4,28 +4,28 @@ export const conditions: {
   [key in ProductConditionEnum]: { name: string; description: string };
 } = {
   [ProductConditionEnum.New]: {
-    name: "Ny i originalförpackning",
+    name: "Originalskick",
     description:
       "Helt ny och oanvänd produkt i sin originalförpackning, med eventuell prislapp kvar",
   },
   [ProductConditionEnum.VeryGood]: {
-    name: "Nyskick utan förpackning",
+    name: "Nyskick",
     description:
-      "Oanvänd produkt utan originalförpackning eller prislapp, helt fri från bruksslitage",
+      "Oanvänd produkt utan originalförpackning eller prislapp, helt fri från slitage.",
   },
   [ProductConditionEnum.Good]: {
-    name: "Gott & bevarat skick",
+    name: "Gott skick",
     description:
-      "Använd produkt med endast mindre slitage som inte påverkar funktion eller hållbarhet, eller ett äldre föremål med naturlig patina",
+      "Använd produkt med endast mindre slitage som inte påverkar funktion eller hållbarhet, eller ett äldre föremål med naturlig patina.",
   },
   [ProductConditionEnum.Okay]: {
     name: "Bruksskick",
     description:
-      "Tydligt använd produkt med synligt slitage men fullt fungerande, där åldrande eller restaurering kan vara en del av dess karaktär",
+      "Tydligt använd produkt med synligt slitage men fullt fungerande. Åldrande eller enklare restaurering kan vara en del av karaktären.",
   },
   [ProductConditionEnum.Bad]: {
-    name: "Slitet men funktionellt",
+    name: "Funktionellt skick",
     description:
-      "Välanvänd produkt med kraftigt slitage men fortsatt brukbar, eventuellt med behov av renovering/service för optimal funktion",
+      "Välanvänd produkt med kraftigt slitage men fortfarande brukbar. Kan behöva service eller renovering för optimal funktion.",
   },
 };
