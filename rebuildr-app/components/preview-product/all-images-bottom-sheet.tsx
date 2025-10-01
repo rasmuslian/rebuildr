e7f4ee3627b7d271920b5ab27d0974889926d25b
Emil Stolpe <emil.stolpe@swace.se>
2025-10-01 16:16:50 +0200

    reb-346: fix aspect ratio so that tall and wide images take up the whoel width while keeping their aspect ratio.

--- rebuildr-app/components/preview-product/all-images-bottom-sheet.tsx
+++ rebuildr-app/components/preview-product/all-images-bottom-sheet.tsx
@@ -1,9 +1,10 @@
 import { BottomSheet } from "@components/bottom-sheet/bottom-sheet";
-import { View } from "react-native";
-import { Image } from "expo-image";
+import { View, Image, Dimensions } from "react-native";
 import { File } from "@/gql/graphql";
 import { borderRadius } from "@constants/sizes";
+import { useEffect, useState } from "react";
 
+const screenWidth = Dimensions.get("window").width - 48;
 type Props = {
   images: File[];
   show: boolean;
@@ -20,20 +21,50 @@ export const AllImagesBottomSheet = ({ images, show, onDismiss }: Props) => {
       screenHeight
       onDismiss={() => onDismiss()}
     >
-      <View style={{ gap: 16, flex: 1, height: "100%" }}>
+      <View
+        style={{
+          gap: 16,
+          flex: 1,
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
         {images.map((image, i) => (
-          <Image
-            key={i}
-            source={image.url}
-            style={{
-              minHeight: 230,
-              aspectRatio: 1,
-              borderRadius: borderRadius.medium,
-            }}
-            contentFit="contain"
-          />
+          <DynamicImage uri={image.url} key={i} />
         ))}
       </View>
     </BottomSheet>
+  );
+};
+
+const DynamicImage = ({ uri }: { uri: string }) => {
+  const [aspectRatio, setAspectRatio] = useState(1);
+
+  useEffect(() => {
+    Image.getSize(uri, (w, h) => {
+      setAspectRatio(w / h);
+    });
+  }, [uri]);
+
+  return (
+    <View
+      style={{
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: borderRadius.medium,
+      }}
+    >
+      <Image
+        source={{ uri }}
+        style={{
+          width: screenWidth,
+          height: undefined,
+          aspectRatio,
+          borderRadius: borderRadius.medium,
+        }}
+        resizeMode="contain"
+      />
+    </View>
   );
 };
