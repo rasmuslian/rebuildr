877e205d9a3efaa344d6987361ea0948b0b96a4e
Aliyawer Qambari <aliyawerqambari@gmail.com>
2025-10-02 13:52:47 +0200

    Prevent indexing and redirect root page to admin

--- rebuildr-admin/next.config.mjs
+++ rebuildr-admin/next.config.mjs
@@ -15,6 +15,15 @@ const config = withPlaiceholder({
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/admin",
+        permanent: true,
+      },
+    ];
+  },
 });
 
 export default config;
