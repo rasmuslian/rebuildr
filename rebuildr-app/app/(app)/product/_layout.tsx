cc092c5f713eb54f7f1756effb4f61a116808463
Emil Stolpe <emil.stolpe@swace.se>
2025-10-07 14:01:26 +0200

    remove unused router since both edit and create is now handled by bottom sheets CreateProductBottomSheet and EditProductBottomSheet

--- rebuildr-app/app/(app)/product/_layout.tsx
+++ rebuildr-app/app/(app)/product/_layout.tsx
@@ -4,7 +4,6 @@ export default function ProductLayout() {
   return (
     <Stack>
       <Stack.Screen name="[productId]" options={{ headerShown: false }} />
-      <Stack.Screen name="edit" options={{ headerShown: false }} />
     </Stack>
   );
 }
