986490af9cbc91b479e2facf55a133a8ec64ec40
Aliyawer Qambari <aliyawerqambari@gmail.com>
2025-11-05 10:44:21 +0100

    Number of columns in desktop footer

--- rebuildr-admin/src/app/api/session/route.ts
+++ rebuildr-admin/src/app/api/session/route.ts
@@ -1,10 +1,20 @@
 import { NextResponse } from "next/server";
 import { getSession } from "@/actions/auth";
+import { defaultSession } from "@/lib/session";
 
 export async function GET() {
   const session = await getSession();
-  return NextResponse.json({
-    isLoggedIn: session.isLoggedIn,
-    accessToken: session.accessToken,
-  });
+  const { isLoggedIn, accessToken } = session;
+
+  if (accessToken) {
+    return NextResponse.json({
+      isLoggedIn,
+      accessToken,
+    });
+  } else {
+    session.destroy();
+    return NextResponse.json({
+      isLoggedIn: defaultSession.isLoggedIn,
+    });
+  }
 }
