457c6307f941906bbac450f7d877d3682010aea3
Emil Stolpe <emil.stolpe@swace.se>
2025-11-05 14:27:23 +0100

    add grafana

--- rebuildr-backend/src/winston.logger.ts
+++ rebuildr-backend/src/winston.logger.ts
@@ -1,5 +1,7 @@
 import { format, transports } from 'winston';
 import { utilities } from 'nest-winston';
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+import LokiTransport = require('winston-loki');
 
 const devLogger = {
   format: format.combine(format.timestamp(), utilities.format.nestLike()),
@@ -8,7 +10,25 @@ const devLogger = {
 
 const prodLogger = {
   format: format.combine(format.timestamp(), utilities.format.nestLike()),
-  transports: [new transports.Console()],
+  transports: [
+    new transports.Console(),
+
+    new LokiTransport({
+      host: 'https://logs-prod-025.grafana.net',
+      labels: {
+        app: 'rebuildr-backend',
+        environment: process.env.NODE_ENV || 'development',
+        api: 'rest',
+      },
+      json: true,
+      basicAuth:
+        '1385783:glc_eyJvIjoiMTU4MDY0NSIsIm4iOiJyZWJ1aWxkci1sb2ctcG9saWN5LXJlYnVpbGRyLWxvZy1wb2xpY3ktdG9rZW4iLCJrIjoiMjY5VnFGVDk4VUk2T0gxYmUzVTZIM3d6IiwibSI6eyJyIjoicHJvZC1ldS1ub3J0aC0wIn19',
+      format: format.json(),
+      replaceTimestamp: true,
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      onConnectionError: (err: any) => console.error(err),
+    }),
+  ],
 };
 
 export const instanceLogger =
