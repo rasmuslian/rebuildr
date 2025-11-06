3b34a24eea148b2b0cb3a9d75b462f7ebf05d651
Emil Stolpe <emil.stolpe@swace.se>
2025-11-06 16:58:13 +0100

    add message if saving results in error

--- rebuildr-app/components/upsert-product/upsert-product-bottom-sheet.tsx
+++ rebuildr-app/components/upsert-product/upsert-product-bottom-sheet.tsx
@@ -24,6 +24,7 @@ import { Details } from "./details";
 import { UPSERT_PRODUCT_PRODUCT_FRAGMENT } from "./queries";
 import { measurementKeys } from "@constants/measurements";
 import { Button } from "@components/buttons/button";
+import { Body } from "@components/typography/text";
 
 export const UPSERT_PRODUCT_BOTTOM_SHEET = gql`
   query UpsertProductBottomSheet($input: GetProductInput!) {
@@ -139,7 +140,7 @@ export const UpsertProductBottomSheet = ({
     UpsertProductBottomSheetQuery,
     UpsertProductBottomSheetQueryVariables
   >(UPSERT_PRODUCT_BOTTOM_SHEET, { variables: { input: { id: productId } } });
-  const [updateProduct, { loading: updatingProduct }] = useMutation<
+  const [updateProduct, { loading: updatingProduct, error }] = useMutation<
     UpsertProductUpdateProductMutation,
     UpsertProductUpdateProductMutationVariables
   >(UPSERT_PRODUCT_UPDATE_PRODUCT);
@@ -530,23 +531,34 @@ export const UpsertProductBottomSheet = ({
       return (
         <View
           style={{
-            gap: 8,
-            flexDirection: "row",
             paddingTop: 24,
+            gap: 6,
           }}
         >
-          <Button
-            label="Redigera"
-            type="tonal"
-            onPress={() => setStep("details")}
-            style={{ flex: 1 }}
-          />
-          <Button
-            label={mode === "create" ? "Publicera" : "Spara"}
-            onPress={onVerifyPreview}
-            style={{ flex: 1 }}
-            loading={updateDraftLoading}
-          />
+          {error && (
+            <Body size="small" color="error">
+              Något gick fel, vänligen gå tillbaka och se över alla fält
+            </Body>
+          )}
+          <View
+            style={{
+              gap: 8,
+              flexDirection: "row",
+            }}
+          >
+            <Button
+              label="Redigera"
+              type="tonal"
+              onPress={() => setStep("details")}
+              style={{ flex: 1 }}
+            />
+            <Button
+              label={mode === "create" ? "Publicera" : "Spara"}
+              onPress={onVerifyPreview}
+              style={{ flex: 1 }}
+              loading={updateDraftLoading}
+            />
+          </View>
         </View>
       );
     }
