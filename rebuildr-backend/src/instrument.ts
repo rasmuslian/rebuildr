49e16b04fcfb0bcfbfeb97d3cca4f63249fb7d75
Emil Stolpe <emil.stolpe@swace.se>
2025-11-12 15:18:00 +0100

    messages can now have document and image files attached to them. Adding necessary loaders and resolvers to Create and Get them

--- rebuildr-backend/src/instrument.ts
+++ rebuildr-backend/src/instrument.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import * as Sentry from '@sentry/nestjs';
-console.log(process.env.NODE_ENV);
+
 Sentry.init({
   dsn: 'https://c17e6469eaf4c052b2644f7d64ce5ec9@o115197.ingest.us.sentry.io/4510306685616128',
   tracesSampleRate: process.env.NODE_ENV === 'production' ? 0.01 : 1.0,
