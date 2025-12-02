f93049964fedd27b1c62602996825c51d29ddedd
DMyrgren <43743237+DMyrgren@users.noreply.github.com>
2025-12-02 10:58:41 +0100

    Add breadcrums (#317)

--- rebuildr-app/components/categories/sub-categories-list/sub-categories-list.mobile.tsx
+++ rebuildr-app/components/categories/sub-categories-list/sub-categories-list.mobile.tsx
@@ -6,6 +6,7 @@ import { Divider } from "@components/dividers/divider";
 import { useFilterProduct } from "@hooks/useFilterProduct";
 import { router } from "expo-router";
 import { Avatar } from "@components/avatar/avatar";
+import { Breadcrums } from "@components/preview-product/breadcrums";
 
 type Props = {
   category: SubCategoriesQuery["category"];
@@ -21,6 +22,7 @@ export function SubCategoriesListMobile({ category }: Props) {
   return (
     <View style={{ marginBottom: 24, gap: 24 }}>
       <View style={{ gap: 16 }}>
+        <Breadcrums parentCategory={category.parent} category={category} />
         <Display size="small">{category?.name}</Display>
         <Body size="large">{category?.description}</Body>
       </View>
