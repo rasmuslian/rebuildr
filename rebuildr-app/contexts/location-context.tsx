0dcb473dfa8b529912a65e7508825b598f28c40d
Aliyawer Qambari <aliyawerqambari@gmail.com>
2025-12-11 11:56:34 +0100

    Set timeout to ask location

--- rebuildr-app/contexts/location-context.tsx
+++ rebuildr-app/contexts/location-context.tsx
@@ -8,7 +8,6 @@ import {
   PermissionStatus,
   getCurrentPositionAsync,
   requestForegroundPermissionsAsync,
-  getForegroundPermissionsAsync,
   LocationObjectCoords,
 } from "expo-location";
 import { View } from "react-native";
@@ -48,21 +47,13 @@ export const LocationProvider = ({ children }: PropsWithChildren) => {
     }
   };
 
-  const getUserCoords = async () => {
-    const { status } = await getForegroundPermissionsAsync();
-    if (status !== PermissionStatus.GRANTED) {
-      setState({ open: true });
-    } else {
-      const { coords } = await getCurrentPositionAsync();
-      setState({ userCoords: coords });
-    }
-  };
-
   useEffect(() => {
-    if (isIos) {
-      getUserCoords();
-    } else {
+    if (!isIos) {
       askForUserCoords();
+    } else {
+      setTimeout(() => {
+        setState({ open: true });
+      }, 2000);
     }
   }, [isIos]);
 
