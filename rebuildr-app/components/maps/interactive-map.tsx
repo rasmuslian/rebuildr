81ce71e7843af48495322569f106f6f7b8c4b4ae
Aliyawer Qambari <aliyawerqambari@gmail.com>
2025-12-17 13:41:06 +0100

    Add zoom controll and center map

--- rebuildr-app/components/maps/interactive-map.tsx
+++ rebuildr-app/components/maps/interactive-map.tsx
@@ -4,9 +4,11 @@ import UserLocationMarker from "@components/maps/user-location-marker";
 import { MapContainer, TileLayer, useMapEvents, useMap } from "react-leaflet";
 import { useDebounceCallback } from "usehooks-ts";
 import { useMapContext, MapProvider } from "@context/map-context";
-import { StyleProp, View, ViewStyle } from "react-native";
+import { StyleProp, View, ViewStyle, Pressable } from "react-native";
 import { Check } from "@components/controls/check";
 import { Label } from "@components/typography/text";
+import { Divider } from "@components/dividers/divider";
+import { Icon } from "@icons/icon";
 
 type Props = {
   style?: StyleProp<ViewStyle>;
@@ -23,7 +25,7 @@ export default function InteractiveMap({ style }: Props) {
 }
 
 const Map = () => {
-  const { state, setState } = useMapContext();
+  const { state } = useMapContext();
 
   return (
     <MapContainer
@@ -36,7 +38,11 @@ const Map = () => {
       maxZoom={16}
       minZoom={5}
     >
-      <Controller />
+      <EventController />
+      <ZoomController />
+      <NavigationController />
+      <PriceController />
+      <ActivePinController />
 
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -45,83 +51,24 @@ const Map = () => {
 
       <UserLocationMarker position={state.center} />
 
-      <View
-        nativeID="show-price"
-        style={{
-          position: "absolute",
-          left: 16,
-          top: 16,
-          width: 107,
-          backgroundColor: "white",
-          paddingHorizontal: 8,
-          paddingVertical: 10,
-          borderRadius: 12,
-          shadowColor: "#000",
-          shadowOpacity: 0.2,
-          shadowRadius: 10,
-          zIndex: 1000,
-          opacity: 0.8,
-        }}
-      >
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            gap: 12,
-          }}
-        >
-          <Check
-            selected={state.showPrice}
-            onPress={() => setState({ showPrice: !state.showPrice })}
-          />
-          <Label size="small">
-            {state.showPrice ? "Göm pris" : "Visa pris"}
-          </Label>
-        </View>
-      </View>
-
       {state.pins.map((pin) => {
         const key = `${pin.location.lat},${pin.location.lng}`;
         return <MapMarker key={key} pin={pin} />;
       })}
-
-      {state.activePin?.popup && (
-        <View
-          nativeID="active-pin-popup"
-          style={{
-            position: "absolute",
-            right: 16,
-            bottom: 16,
-            width: 149,
-            backgroundColor: "white",
-            paddingHorizontal: 8,
-            paddingVertical: 10,
-            borderRadius: 18,
-            shadowColor: "#000",
-            shadowOpacity: 0.2,
-            shadowRadius: 10,
-            zIndex: 1000,
-          }}
-        >
-          {state.activePin.popup}
-        </View>
-      )}
     </MapContainer>
   );
 };
 
-const Controller = () => {
+const EventController = () => {
   const { state, setState } = useMapContext();
   const map = useMap();
 
   const boundDebounce = useDebounceCallback(() => {
     map.whenReady(() => {
       const bounds = map.getBounds();
-      if (bounds) {
-        const northEast = bounds.getNorthEast();
-        const southWest = bounds.getSouthWest();
-        setState({ bounds: { northEast, southWest } });
-      }
+      const northEast = bounds.getNorthEast();
+      const southWest = bounds.getSouthWest();
+      setState({ bounds: { northEast, southWest } });
     });
   }, 500);
 
@@ -139,8 +86,10 @@ const Controller = () => {
       const target = e.originalEvent.target as HTMLElement;
 
       if (
-        target.closest("#active-pin-popup") ||
-        target.closest("#show-price")
+        target.closest("#active-pin-controller") ||
+        target.closest("#price-controller") ||
+        target.closest("#zoom-controller") ||
+        target.closest("#navigation-controller")
       ) {
         return;
       }
@@ -159,4 +108,161 @@ const Controller = () => {
   }, [state.center]);
 
   return null;
+};
+
+const ZoomController = () => {
+  const map = useMap();
+
+  return (
+    <View
+      nativeID="zoom-controller"
+      style={{
+        position: "absolute",
+        right: 16,
+        top: 16,
+        backgroundColor: "white",
+        borderRadius: 12,
+        shadowColor: "#000",
+        shadowOpacity: 0.2,
+        shadowRadius: 10,
+        zIndex: 1000,
+        opacity: 0.8,
+      }}
+    >
+      <Pressable
+        onPress={() => map.zoomIn()}
+        style={{
+          padding: 8,
+          width: 40,
+          height: 40,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Icon icon="+" size={18} />
+      </Pressable>
+
+      <Divider />
+
+      <Pressable
+        onPress={() => map.zoomOut()}
+        style={{
+          padding: 8,
+          width: 40,
+          height: 40,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Icon icon="-" size={18} />
+      </Pressable>
+    </View>
+  );
+};
+
+const PriceController = () => {
+  const { state, setState } = useMapContext();
+
+  return (
+    <View
+      nativeID="price-controller"
+      style={{
+        position: "absolute",
+        left: 16,
+        top: 16,
+        width: 107,
+        backgroundColor: "white",
+        paddingHorizontal: 8,
+        paddingVertical: 10,
+        borderRadius: 12,
+        shadowColor: "#000",
+        shadowOpacity: 0.2,
+        shadowRadius: 10,
+        zIndex: 1000,
+        opacity: 0.8,
+      }}
+    >
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          gap: 12,
+        }}
+      >
+        <Check
+          selected={state.showPrice}
+          onPress={() => setState({ showPrice: !state.showPrice })}
+        />
+        <Label size="small">{state.showPrice ? "Göm pris" : "Visa pris"}</Label>
+      </View>
+    </View>
+  );
+};
+
+const ActivePinController = () => {
+  const { state } = useMapContext();
+  const activePin = state.activePin;
+  if (!activePin) return null;
+
+  return (
+    <View
+      nativeID="active-pin-controller"
+      style={{
+        position: "absolute",
+        right: 16,
+        bottom: 16,
+        width: 149,
+        backgroundColor: "white",
+        paddingHorizontal: 8,
+        paddingVertical: 10,
+        borderRadius: 18,
+        shadowColor: "#000",
+        shadowOpacity: 0.2,
+        shadowRadius: 10,
+        zIndex: 1000,
+      }}
+    >
+      {activePin.popup}
+    </View>
+  );
+};
+
+const NavigationController = () => {
+  const { state } = useMapContext();
+  const map = useMap();
+
+  return (
+    <View
+      nativeID="navigation-controller"
+      style={{
+        position: "absolute",
+        right: 16,
+        top: 110,
+        backgroundColor: "white",
+        borderRadius: 12,
+        shadowColor: "#000",
+        shadowOpacity: 0.2,
+        shadowRadius: 10,
+        zIndex: 1000,
+        opacity: 0.8,
+      }}
+    >
+      <Pressable
+        onPress={() => {
+          map.whenReady(() => {
+            map.flyTo(state.center, 13, { animate: true, duration: 0.5 });
+          });
+        }}
+        style={{
+          padding: 8,
+          width: 40,
+          height: 40,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Icon icon="navigation" size={18} />
+      </Pressable>
+    </View>
+  );
 };
