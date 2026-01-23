ca72bbd16c2bff3982b86fa856a626b3ef9be250
Emil Stolpe <emil.stolpe@swace.se>
2026-01-23 16:46:22 +0100

    reb-664: explanatory text for conditions

---
+++ rebuildr-app/components/sell-product/explain-conditions-bottom-sheet.tsx
@@ -0,0 +1,80 @@
+import { BottomSheet } from "@components/bottom-sheet/bottom-sheet";
+import { Header } from "@components/navigation/headers/header";
+import { Popup } from "@components/popup/popup";
+import { Body, Display, Headline } from "@components/typography/text";
+import { conditions } from "@constants/conditions";
+import { useScreenType } from "@hooks/useScreenType";
+import { View } from "react-native";
+
+type Props = {
+  show: boolean;
+  onDismiss: () => void;
+};
+
+export const ExplainConditionsBottomSheet = ({ show, onDismiss }: Props) => {
+  const { isDesktop } = useScreenType();
+
+  const content = (
+    <View
+      style={[
+        { justifyContent: "space-between", flex: 1, marginBottom: 16 },
+        isDesktop && { marginBottom: 0 },
+      ]}
+    >
+      <View style={{ gap: 24, marginVertical: 24 }}>
+        <Display size="small">Ange skick</Display>
+        <Body size="medium">
+          Utgå från produktens funktion. Beskriv slitage, defekter eller annan
+          info som underlättar för en köpare i annonsfältet Bra att veta.
+        </Body>
+        {Object.values(conditions).map((value, i) => {
+          return (
+            <View style={{ gap: 12 }} key={i}>
+              <Headline size="small">{value.name}</Headline>
+              <Body size="medium">{value.description}</Body>
+            </View>
+          );
+        })}
+      </View>
+    </View>
+  );
+
+  if (isDesktop) {
+    return (
+      <Popup open={show} onClose={onDismiss}>
+        <View
+          style={{
+            paddingTop: 16,
+            paddingBottom: 24,
+            paddingHorizontal: 24,
+            justifyContent: "center",
+          }}
+        >
+          <Header
+            title="Ange skick"
+            showBackButton={false}
+            showDivider
+            ctas={[
+              {
+                icon: "X",
+                onPress: onDismiss,
+              },
+            ]}
+          />
+          <View style={{ padding: 48, paddingTop: 24 }}>{content}</View>
+        </View>
+      </Popup>
+    );
+  }
+
+  return (
+    <BottomSheet
+      open={show}
+      name="conditions explanation"
+      title="Ange skick"
+      onDismiss={onDismiss}
+    >
+      {content}
+    </BottomSheet>
+  );
+};
