c28cc2d90e762748221a1c070afca32365f5ba5d
Emil Stolpe <emil.stolpe@swace.se>
2026-01-26 16:05:07 +0100

    reb-671: adjust color field and remove color toggle

--- rebuildr-app/components/product/color-section.tsx
+++ rebuildr-app/components/product/color-section.tsx
@@ -1,8 +1,7 @@
 import { ColorTypeEnum } from "@/gql/graphql";
-import { Toggle } from "@components/controls/toggle";
 import { SelectInput } from "@components/forms/selectInput";
 import { TextInput } from "@components/forms/textInput";
-import { Label, Title } from "@components/typography/text";
+import { Label } from "@components/typography/text";
 import { colorTypes, ColorTypesType } from "@constants/product-color-types";
 import { useState } from "react";
 import { View } from "react-native";
@@ -13,12 +12,14 @@ type Props = {
   onChange: (color: string, type: ColorTypeEnum) => void;
 };
 
-export const ColorSection = ({ color, type, onChange }: Props) => {
-  const [selected, setSelected] = useState(() => {
-    return !!color;
-  });
-
+export const ColorSection = ({ color: _color, type, onChange }: Props) => {
+  const [color, setColor] = useState(_color ?? "");
   const selectedColor = color ?? "";
+
+  const onChangeColor = (c: string) => {
+    setColor(c);
+    onChange(c, type);
+  };
 
   return (
     <View style={{ gap: 16 }}>
@@ -26,46 +27,31 @@ export const ColorSection = ({ color, type, onChange }: Props) => {
         style={{
           flexDirection: "row",
           justifyContent: "space-between",
-          alignItems: "center",
-          paddingVertical: 18,
+          gap: 16,
+          alignItems: "flex-end",
         }}
       >
-        <Title size="medium">Lägg till färg</Title>
-        <Toggle value={selected} onPress={() => setSelected(!selected)} />
-      </View>
-      {selected && (
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-between",
-            gap: 16,
-            alignItems: "flex-end",
-          }}
-        >
-          <View style={{ minWidth: 213, gap: 4 }}>
-            <Label size="medium">Ange färgkod</Label>
-            <TextInput
-              value={selectedColor}
-              placeholder={type === ColorTypeEnum.Ncs ? "S 3010-Y30R" : "Grön"}
-              onChange={(c) => {
-                onChange(c, type);
-              }}
-            />
-          </View>
-          <View style={{ flex: 1 }}>
-            <SelectInput
-              value={type}
-              options={Object.keys(colorTypes).map((o) => ({
-                label: colorTypes[o as ColorTypesType].text,
-                value: o,
-              }))}
-              onSelect={(unit) => {
-                onChange("", unit as ColorTypeEnum);
-              }}
-            />
-          </View>
+        <View style={{ minWidth: 213, gap: 4 }}>
+          <Label size="medium">Ange färgkod</Label>
+          <TextInput
+            value={selectedColor}
+            placeholder={type === ColorTypeEnum.Ncs ? "S 3010-Y30R" : "Grön"}
+            onChange={onChangeColor}
+          />
         </View>
-      )}
+        <View style={{ flex: 1 }}>
+          <SelectInput
+            value={type}
+            options={Object.keys(colorTypes).map((o) => ({
+              label: colorTypes[o as ColorTypesType].text,
+              value: o,
+            }))}
+            onSelect={(unit) => {
+              onChange("", unit as ColorTypeEnum);
+            }}
+          />
+        </View>
+      </View>
     </View>
   );
 };
