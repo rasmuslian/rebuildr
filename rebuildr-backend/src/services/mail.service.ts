b9f7535a2c3101f2c12551c898fc1300f4d8e47d
Aliyawer Qambari <aliyawerqambari@gmail.com>
2026-03-09 14:11:32 +0100

    Mail from copy fix

--- rebuildr-backend/src/services/mail.service.ts
+++ rebuildr-backend/src/services/mail.service.ts
@@ -57,7 +57,7 @@ export class MailService {
       url: 'https://api.eu.mailgun.net',
     });
     this.baseUrl = process.env.WEB_BASE_URL;
-    this.from = 'ReuildR <noreply@rebuildr.se>';
+    this.from = 'RebuildR <noreply@rebuildr.se>';
   }
 
   async onModuleInit() {
