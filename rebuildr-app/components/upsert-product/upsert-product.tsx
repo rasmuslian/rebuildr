2f70c4e472a6ffeb3a375ce3491dd3103fe41604
Emil Stolpe <emil.stolpe@swace.se>
2026-03-26 08:37:43 +0100

    fix issue where changing image or adding image would not result in save and thus backend used old data when analyzing images

--- rebuildr-app/components/upsert-product/upsert-product.tsx
+++ rebuildr-app/components/upsert-product/upsert-product.tsx
@@ -487,11 +487,9 @@ export const UpsertProduct = ({
 
   const onAnalyzeImages = async () => {
     if (!data) return;
-    // If no images are saved in the backend yet, save them first
-    if (!data.product.images.length) {
-      const saved = await update();
-      if (!saved) return;
-    }
+    //save first so that all images are available on the backend
+    const saved = await update();
+    if (!saved) return;
 
     await analyzeImages({ variables: { input: { productId } } });
   };
