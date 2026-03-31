327c0c32515e95f59595efb5881aba812d3ffbcc
Emil Stolpe <emil.stolpe@swace.se>
2026-03-31 16:12:11 +0200

    remove pull to refresh since it did not work

--- rebuildr-app/app/(app)/product-list/[userId].tsx
+++ rebuildr-app/app/(app)/product-list/[userId].tsx
@@ -51,7 +51,7 @@ export default function ProductList() {
 
   const { isDesktop } = useScreenType();
 
-  const { data, loading, fetchMore, refetch } = useQuery<
+  const { data, loading, fetchMore } = useQuery<
     ProductListQuery,
     ProductListQueryVariables
   >(PRODUCT_LIST, {
@@ -60,7 +60,6 @@ export default function ProductList() {
       limit: PRODUCTS_PER_PAGE,
       offset: 0,
     },
-    fetchPolicy: "network-only",
   });
 
   const onShowMore = async () => {
@@ -104,9 +103,7 @@ export default function ProductList() {
           <Header title={sellerIsMe ? "Dina annonser" : "Annonser"} />
         )
       }
-      // loading={loading}
-      onRefresh={refetch}
-      refreshing={loading}
+      loading={loading}
     >
       {numberOfProducts > 0 ? (
         <AdGridSection
