8079acf661a953b13719a0764c207509b63491c0
Emil Stolpe <emil.stolpe@swace.se>
2026-04-15 09:26:22 +0200

    insert script

--- rebuildr-app/index.d.ts
+++ rebuildr-app/index.d.ts
@@ -3,7 +3,5 @@ declare module "*.jpeg";
 declare module "*.svg";
 
 interface Window {
-  gtag: (command: string, ...args: unknown[]) => void;
   dataLayer: unknown[];
-  fbq: (command: string, ...args: unknown[]) => void;
 }
