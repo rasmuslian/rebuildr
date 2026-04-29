3a774f572fd756f768f94826a94b63cbaeae94ac
Emil Stolpe <emil.stolpe@swace.se>
2026-04-29 15:47:07 +0200

    replace ActivityIndicator with Lottie animation as default loader

--- rebuildr-app/components/loading-spinner/loading-spinner.tsx
+++ rebuildr-app/components/loading-spinner/loading-spinner.tsx
@@ -1,4 +1,5 @@
-import { View, ActivityIndicator, ViewStyle } from "react-native";
+import { View, ViewStyle } from "react-native";
+import LottieView from "lottie-react-native";
 
 type Props = {
   style?: ViewStyle;
@@ -11,7 +12,12 @@ export const LoadingSpinner = ({ style }: Props) => {
         style,
       ]}
     >
-      <ActivityIndicator />
+      <LottieView
+        source={require("../../assets/images/loader-rebuildr.lottie")}
+        autoPlay
+        loop
+        style={{ width: 100, height: 100 }}
+      />
     </View>
   );
 };
