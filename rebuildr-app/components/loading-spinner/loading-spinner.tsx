bf46eb7be12b54248a41697f3fa0360398ebd1db
Emil Stolpe <emil.stolpe@swace.se>
2026-06-04 14:00:47 +0200

    create custom loader with pure react native components instead of using lottie. This is to improve preformance by reducing the size of the app

--- rebuildr-app/components/loading-spinner/loading-spinner.tsx
+++ rebuildr-app/components/loading-spinner/loading-spinner.tsx
@@ -1,10 +1,30 @@
-import { View, ViewStyle } from "react-native";
-import LottieView from "lottie-react-native";
+import { borderRadius } from "@constants/sizes";
+import { useEffect, useRef } from "react";
+import { Animated, Easing, Image, View, ViewStyle } from "react-native";
 
 type Props = {
   style?: ViewStyle;
 };
+
 export const LoadingSpinner = ({ style }: Props) => {
+  const rotation = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.loop(
+      Animated.timing(rotation, {
+        toValue: 1,
+        duration: 1500,
+        easing: Easing.linear,
+        useNativeDriver: false,
+      }),
+    ).start();
+  }, []);
+
+  const spin = rotation.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["0deg", "360deg"],
+  });
+
   return (
     <View
       style={[
@@ -12,13 +32,23 @@ export const LoadingSpinner = ({ style }: Props) => {
         style,
       ]}
     >
-      <LottieView
-        source={require("../../assets/images/loader-rebuildr.lottie")}
-        autoPlay
-        loop
-        webStyle={{ width: 50 }}
-        style={{ width: 50, height: 50 }}
-      />
+      <View
+        style={{
+          width: 30,
+          height: 30,
+          borderRadius: borderRadius.xSmall,
+          backgroundColor: "rgba(0,0,0,0.28)",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Animated.View style={{ transform: [{ rotate: spin }] }}>
+          <Image
+            source={require("../../assets/images/loader-icon.png")}
+            style={{ width: 90, height: 90 }}
+          />
+        </Animated.View>
+      </View>
     </View>
   );
 };
